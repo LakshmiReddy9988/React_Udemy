8f72f9cd3b56a77f74520f3884b59017d004c101
LakshmiReddy9988 <reddyb9052@gmail.com>
2024-03-20 18:34:26 +0530

    Steps App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+
+  const messages = ["Welcome to step1", "Click next to proceed", "Thank you!"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step} : {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          onClick={() => {
+            if (step > 1) setStep((step) => step - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          onClick={() => {
+            if (step < 3) setStep((step) => step + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
