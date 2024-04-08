057f0a294f1a6cb6eb10c6ad2665535d163c4f02
LakshmiReddy9988 <160628275+LakshmiReddy9988@users.noreply.github.com>
2024-04-08 10:03:18 +0530

    Currency Converter - Challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setConverted] = useState("");
+
+  useEffect(() => {
+    async function convert() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+      );
+      const data = await res.json();
+      setConverted(data.rates[toCur]);
+    }
+
+    convert();
+  }, [amount, fromCur, toCur]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+
+      <Dropdown cur={fromCur} setCur={setFromCur} />
+
+      <Dropdown cur={toCur} setCur={setToCur} />
+
+      <p>
+        {converted} {toCur}
+      </p>
     </div>
+  );
+}
+
+function Dropdown({ cur, setCur }) {
+  return (
+    <select value={cur} onChange={(e) => setCur(e.target.value)}>
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <option value="CAD">CAD</option>
+      <option value="INR">INR</option>
+    </select>
   );
 }
 
